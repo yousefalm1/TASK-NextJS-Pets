5be50b92fd7271df43651a041280395ef1309606
Octowl <alsaff1987@gmail.com>
2024-10-01 14:41:22 +0300

    move heading

--- src/app/Components/PetsList.js
+++ src/app/Components/PetsList.js
@@ -4,38 +4,32 @@ function PetsList({ pets }) {
   const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
 
   return (
-    <section id="doctors" className="doctor-section pt-140">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xxl-5 col-xl-6 col-lg-7">
-            <div className="section-title text-center mb-30">
-              <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
-                Fur-ends
-              </h1>
-              <div className="input-group rounded">
-                <input
-                  type="search"
-                  className="form-control rounded"
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-describedby="search-addon"
-                />
-              </div>
-              <br />
-              Type:
-              <select className="form-select" defaultValue={""}>
-                <option value="">All</option>
-                <option value="Cat">Cat</option>
-                <option value="Dog">Dog</option>
-                <option value="Rabbit">Rabbit</option>
-              </select>
+    <div>
+      <div className="row justify-content-center">
+        <div className="col-xxl-5 col-xl-6 col-lg-7">
+          <div className="section-title text-center mb-30">
+            <div className="input-group rounded">
+              <input
+                type="search"
+                className="form-control rounded"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
             </div>
+            <br />
+            Type:
+            <select className="form-select" defaultValue={""}>
+              <option value="">All</option>
+              <option value="Cat">Cat</option>
+              <option value="Dog">Dog</option>
+              <option value="Rabbit">Rabbit</option>
+            </select>
           </div>
         </div>
-
-        <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">{petList}</div>
       </div>
-    </section>
+      <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">{petList}</div>
+    </div>
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,9 @@ import pets from "./data/pets";
 export default function Home() {
   return (
     <div className="mx-auto max-w-6xl">
+      <h1 className="leading-relaxed font-primary font-extrabold text-4xl text-center text-palette-primary mt-4 py-2 sm:py-4">
+        Fur-ends
+      </h1>
       <PetsList pets={pets} />
     </div>
   );
