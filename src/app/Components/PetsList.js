017ad77c6aeb47fe66a92e28c16dce51f2503ca6
Octowl <alsaff1987@gmail.com>
2024-10-01 15:02:54 +0300

    add tailwind form styling

--- src/app/Components/PetsList.js
+++ src/app/Components/PetsList.js
@@ -4,22 +4,21 @@ function PetsList({ pets }) {
   const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
 
   return (
-    <div>
-      <div className="row justify-content-center">
-        <div className="col-xxl-5 col-xl-6 col-lg-7">
-          <div className="section-title text-center mb-30">
-            <div className="input-group rounded">
-              <input
-                type="search"
-                className="form-control rounded"
-                placeholder="Search"
-                aria-label="Search"
-                aria-describedby="search-addon"
-              />
-            </div>
-            <br />
-            Type:
-            <select className="form-select" defaultValue={""}>
+    <>
+      <div>
+        <div className="flex justify-start space-x-2 w-full font-primary">
+          <div className="flex flex-col items-start space-y-1 flex-grow">
+            <input
+              type="search"
+              placeholder="search"
+              className="text-gray-900 form-input border border-gray-300 w-full rounded-sm focus:border-palette-light focus:ring-palette-light"
+            />
+          </div>
+          <div className="flex flex-col items-start space-y-1 flex-grow-0">
+            <select
+              defaultValue={""}
+              className="form-select border border-gray-300 rounded-sm w-full text-gray-900 focus:border-palette-light focus:ring-palette-light"
+            >
               <option value="">All</option>
               <option value="Cat">Cat</option>
               <option value="Dog">Dog</option>
@@ -29,7 +28,7 @@ function PetsList({ pets }) {
         </div>
       </div>
       <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">{petList}</div>
-    </div>
+    </>
   );
 }
 
