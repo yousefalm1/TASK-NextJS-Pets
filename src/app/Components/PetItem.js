d0cdd8f6cd4f08048d7ea75ae61a6d4929629790
Octowl <alsaff1987@gmail.com>
2024-10-01 13:00:22 +0300

    add pet content

---
+++ src/app/Components/PetItem.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+function PetItem({ pet }) {
+  return (
+    <div className="h-120 w-72 rounded shadow-lg mx-auto border border-palette-lighter">
+      <div className="h-72 border-b-2 border-palette-lighter relative">
+        <Image
+          alt={pet.name}
+          src={pet.image}
+          layout="fill"
+          className="transform duration-500 ease-in-out hover:scale-110"
+        />
+      </div>
+      <div className="h-48 relative">
+        <div className="font-primary text-palette-primary text-2xl pt-4 px-4 font-semibold">
+          {pet.name}
+        </div>
+        <div className="text-lg text-gray-600 p-4 font-primary font-light">
+          {pet.description}
+        </div>
+        <div
+          className="text-palette-dark font-primary font-medium text-base mb-4 pl-8 pr-4 pb-1 pt-2 bg-palette-lighter 
+            rounded-tl-sm triangle"
+        >
+          <button type="button" className="btn btn-info">
+            Pet
+          </button>
+          <button type="button" class="btn btn-info  m-2">
+            Adopt
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PetItem;
