6a2dea7b2ae61046be43834327bc95cc2df6befe
Octowl <alsaff1987@gmail.com>
2024-10-01 13:21:08 +0300

    add new theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,27 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      height: theme => ({
+        '112': '28rem',
+        '120': '30rem',
+      }),
+      minHeight: theme => ({
+        '80': '20rem',
+      }),
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        palette: {
+          lighter: '#F5F3FF',
+          light: '#DDD6FE',
+          primary: '#5B21B6',
+          dark: '#4C1D95',
+        },
       },
+      fontFamily: {
+        primary: ['"Josefin Sans"']
+      }
     },
   },
   plugins: [],
